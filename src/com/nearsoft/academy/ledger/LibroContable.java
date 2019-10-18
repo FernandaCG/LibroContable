aa5e72b45cb28ac10faf099d4614418d1d5b8562
FernandaCG <fernandai.camposg@gmail.com>
2019-10-18 17:24:31 -0500

    Detectando fecha en archivos

--- src/com/nearsoft/academy/ledger/LibroContable.java
+++ src/com/nearsoft/academy/ledger/LibroContable.java
@@ -7,13 +7,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
+import com.nearsoft.academy.ledger.Entity.Transaction;
+
 public class LibroContable {
 
     List<String> pathFiles = new ArrayList<>();
+    List<Transaction> Operations = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         LibroContable libroContable = new LibroContable();
-        libroContable.getInfo();
+        //libroContable.printCommand();
+        libroContable.getInfoToStructure();
     }
 
     public LibroContable() throws IOException {
@@ -24,7 +29,12 @@ public class LibroContable {
         return pathFiles;
     }
 
+    public void printCommand() throws IOException {
+        getInfo();
+    }
+
     public void initFiles() throws IOException {
+        //Leer del argumento de linea de comando -f
         FileReader fr = new FileReader("../resources/index.ledger");
         BufferedReader br = new BufferedReader(fr);
         String actualLine;
@@ -37,17 +47,37 @@ public class LibroContable {
 
     public void getInfo() throws IOException {
         String nextFile=null;
-        System.out.println(pathFiles.size());
         for(int i=0; i<pathFiles.size(); i++){
             nextFile = pathFiles.get(i).split(" ")[1];
             FileReader fr = new FileReader("../resources/"+nextFile);
             BufferedReader br = new BufferedReader(fr);
             String actualLine;
             while ((actualLine = br.readLine()) != null){
-                System.out.println(actualLine);
+                if(!actualLine.contains(";")){
+                    System.out.println(actualLine);
+                }
             }
             br.close();
             fr.close();
+        }
+    }
+
+    public void getInfoToStructure() throws IOException {
+        String nextFile=null;
+        Transaction transaction = new Transaction();
+        for(int i=0; i<pathFiles.size(); i++){
+            nextFile = pathFiles.get(i).split(" ")[1];
+            FileReader fr = new FileReader("../resources/"+nextFile);
+            BufferedReader br = new BufferedReader(fr);
+            String actualLine;
+            while ((actualLine = br.readLine()) != null){
+                if(!actualLine.contains(";") && actualLine.contains("/")){
+                    transaction.setAccount(actualLine.split(" ")[0]);
+                }
+            }
+            br.close();
+            fr.close();
+            System.out.println("Transactions"+transaction.getAccount());
         }
     }
 }
